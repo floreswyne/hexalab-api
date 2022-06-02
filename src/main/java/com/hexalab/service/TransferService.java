167bebf25fe0f2039577a260818246005c0c790c
Vinícius Flores <vinicius.camargo.flores@gmail.com>
2022-06-02 11:10:48 -0300

    feat: Criada lógica basica de transferencias e depositos

--- src/main/java/com/hexalab/service/TransferService.java
+++ src/main/java/com/hexalab/service/TransferService.java
@@ -1,5 +1,6 @@
 package com.hexalab.service;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -9,7 +10,9 @@ import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.hexalab.entity.AccountEntity;
 import com.hexalab.entity.TransferEntity;
+import com.hexalab.enums.TransferTypeEnum;
 import com.hexalab.repository.TransferRepository;
 
 @Service
@@ -20,6 +23,30 @@ public class TransferService {
 	
 	@Transactional
 	public TransferEntity save(TransferEntity transfer) {
+		if (transfer.getType().equals(TransferTypeEnum.TRANSFER)) {
+			AccountEntity sender = transfer.getSender();
+			BigDecimal differenceValueSender = sender.getBalance().subtract(transfer.getValue());
+			
+			if (differenceValueSender.signum() < 0) {
+				return null;
+			}
+			
+			sender.setBalance(differenceValueSender);
+			
+			AccountEntity receiver = transfer.getReceiver();
+			
+			receiver.setBalance(receiver.getBalance().add(transfer.getValue()));
+			
+			transfer.setSender(sender);
+			transfer.setReceiver(receiver);
+		} else if (transfer.getType().equals(TransferTypeEnum.DEPOSIT)) {
+			AccountEntity receiver = transfer.getReceiver();
+			
+			receiver.setBalance(receiver.getBalance().add(transfer.getValue()));
+			
+			transfer.setReceiver(receiver);
+		}
+		
 		return transferRepository.save(transfer);
 	}
 	
