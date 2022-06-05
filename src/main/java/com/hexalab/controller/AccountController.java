07243d47bc647695cddad77f89de078b112e8ad4
Vinícius Flores <vinicius.camargo.flores@gmail.com>
2022-06-05 16:49:13 -0300

    refac: Retirado metodo não utilizado de Controller

--- src/main/java/com/hexalab/controller/AccountController.java
+++ src/main/java/com/hexalab/controller/AccountController.java
@@ -3,19 +3,14 @@ package com.hexalab.controller;
 import java.util.List;
 import java.util.UUID;
 
-import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.hexalab.dto.input.AccountInputDTO;
 import com.hexalab.dto.output.AccountOutputDTO;
 import com.hexalab.entity.AccountEntity;
 import com.hexalab.service.AccountService;
@@ -37,21 +32,6 @@ public class AccountController {
 	public ResponseEntity<Object> findAll() {
 		List<AccountOutputDTO> accounts = accountService.findAll().stream().map(AccountEntity::toOutputDTO).toList();
 		return ResponseEntity.status(HttpStatus.FOUND).body(accounts);
-	}
-
-	@PostMapping
-	public ResponseEntity<Object> save(@RequestBody @Valid AccountInputDTO dto) {
-		AccountEntity newAccount = dto.toEntity();
-		AccountOutputDTO createdAccount = accountService.save(newAccount).toOutputDTO();
-		return ResponseEntity.status(HttpStatus.CREATED).body(createdAccount);
-	}
-
-	@PostMapping(value = "/accounts")
-	public ResponseEntity<Object> saveAll(@RequestBody List<@Valid AccountInputDTO> dtos) {
-		List<AccountEntity> newAccounts = dtos.stream().map(AccountInputDTO::toEntity).toList();
-		List<AccountOutputDTO> createdAccounts = accountService.saveAll(newAccounts).stream()
-				.map(AccountEntity::toOutputDTO).toList();
-		return ResponseEntity.status(HttpStatus.CREATED).body(createdAccounts);
 	}
 
 }
